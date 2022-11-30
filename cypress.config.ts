2f26fcb956962f1a4179064f2504bc15097a3ca1
“pourdanisj” <pourdanisj@vmware.com>
2022-11-30 14:24:55 +0200

    upload cypress video

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,25 +1,25 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   env: {
     coverage: false,
   },
-  projectId: '7vknyb',
+  projectId: "7vknyb",
   retries: {
     openMode: 0,
     runMode: 10,
   },
-  video: false,
+  video: true,
   viewportHeight: 550,
   viewportWidth: 700,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      config.env.EXTERNAL_API_URL = 'https://gorest.co.in/public/v2';
-      return require('./cypress/plugins/index.ts')(on, config)
+      config.env.EXTERNAL_API_URL = "https://gorest.co.in/public/v2";
+      return require("./cypress/plugins/index.ts")(on, config);
     },
-    baseUrl: 'http://localhost:3000',
-    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    baseUrl: "http://localhost:3000",
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
   },
-})
+});
